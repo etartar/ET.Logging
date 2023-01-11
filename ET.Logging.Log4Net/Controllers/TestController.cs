6f8eae7b093f3d662fc97682fc8727316b5cdb14
Emir TARTAR <emirtartar35@gmail.com>
2023-01-12 00:18:35 +0300

    Add project files.

---
+++ ET.Logging.Log4Net/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using ET.Logging.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ET.Logging.Log4Net.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ICustomLogger _logger;
+
+        public TestController(ICustomLogger logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult SayHi()
+        {
+            _logger.Info("Hello");
+            return Ok();
+        }
+    }
+}
---
+++ ET.Logging/Log4Net/Log4NetLogger.cs
@@ -0,0 +1,103 @@
+﻿using ET.Logging.Abstract;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+using log4net.Repository.Hierarchy;
+using System.Reflection;
+using System.Xml;
+
+namespace ET.Logging.Log4Net
+{
+    public class Log4NetLogger : ICustomLogger
+    {
+        internal readonly ILog _logger;
+        internal readonly ILoggerRepository _loggerRepository;
+
+        public Log4NetLogger()
+        {
+            _logger = LogManager.GetLogger(typeof(Log4NetLogger));
+
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(File.OpenRead("log4net.config"));
+            ILoggerRepository loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(Hierarchy));
+            XmlConfigurator.Configure(loggerRepository, log4netConfig["log4net"]);
+
+            _loggerRepository = loggerRepository;
+        }
+
+        public void Debug(object message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            _logger.Debug(message, exception);
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            _logger.DebugFormat(format, args);
+        }
+
+        public void Error(object message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            _logger.Error(message, exception);
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            _logger.ErrorFormat(format, args);
+        }
+
+        public void Fatal(object message)
+        {
+            _logger.Fatal(message);
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            _logger.Fatal(message, exception);
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            _logger.FatalFormat(format, args);
+        }
+
+        public void Info(object message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            _logger.Info(message, exception);
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            _logger.InfoFormat(format, args);
+        }
+
+        public void Warn(object message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            _logger.Warn(message, exception);
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            _logger.WarnFormat(format, args);
+        }
+    }
+}
