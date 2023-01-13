9d7581a282311470c2a10ac7da3c6527809ecbb1
Emir TARTAR <emirtartar35@gmail.com>
2023-01-13 18:59:48 +0300

    Add Serilog implementation for ElasticSearch

---
+++ ET.Logging/Serilog/Logger/ElasticSearchLogger.cs
@@ -0,0 +1,27 @@
+﻿using ET.Logging.Serilog.ConfigurationModels;
+using ET.Logging.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Elasticsearch;
+using Serilog.Sinks.Elasticsearch;
+
+namespace ET.Logging.Serilog.Logger
+{
+    public class ElasticSearchLogger : LoggerServiceBase
+    {
+        public ElasticSearchLogger(IConfiguration configuration)
+        {
+            var logConfiguration = configuration.GetSection("SeriLogConfigurations:ElasticSearchConfiguration")
+                .Get<ElasticSearchConfiguration>() ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+            Logger = new LoggerConfiguration()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(logConfiguration.ConnectionString))
+                {
+                    AutoRegisterTemplate = logConfiguration.AutoRegisterTemplate,
+                    AutoRegisterTemplateVersion = (AutoRegisterTemplateVersion)logConfiguration.AutoRegisterTemplateVersion,
+                    CustomFormatter = new ExceptionAsObjectJsonFormatter(renderMessage: logConfiguration.RenderMessage)
+                })
+                .CreateLogger();
+        }
+    }
+}
