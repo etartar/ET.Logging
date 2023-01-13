115979bf9444b333009272f283ae7c3f74827f1e
Emir TARTAR <emirtartar35@gmail.com>
2023-01-13 18:12:19 +0300

    Add Serilog implementation for MongoDb

---
+++ ET.Logging/Serilog/Logger/MongoDbLogger.cs
@@ -0,0 +1,24 @@
+﻿using ET.Logging.Serilog.ConfigurationModels;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Serilog;
+
+namespace ET.Logging.Serilog.Logger
+{
+    public class MongoDbLogger : LoggerServiceBase
+    {
+        public MongoDbLogger(IConfiguration configuration)
+        {
+            var logConfiguration = configuration.GetSection("SeriLogConfigurations:MongoDbConfiguration").Get<MongoDbConfiguration>();
+
+            Logger = new LoggerConfiguration()
+                .WriteTo.MongoDBBson(cfg =>
+                {
+                    var client = new MongoClient(logConfiguration.ConnectionString);
+                    var mongoDbInstance = client.GetDatabase(logConfiguration.Collection);
+                    cfg.SetMongoDatabase(mongoDbInstance);
+                })
+                .CreateLogger();
+        }
+    }
+}
